7316206c2f1f129142fabfa236c8fa8efe7c7479
Russell Uzal <russell.uzal@gmail.com>
2021-03-28 12:57:45 -0500

    changed smokeFunctional (used function)

--- test/smoke/smokeFunctional.js
+++ test/smoke/smokeFunctional.js
@@ -1,5 +1,6 @@
 import sel from '../../data/selectors';
 import {name, gender, age, story} from '../../data/testData';
+import inputValues4 from '../../helpers/methods';
 
 describe('Required fields and story created', function () {
 
@@ -20,13 +21,9 @@ describe('Required fields and story created', function () {
     it('TC-027 User is redirected to the story page', function () {
         browser.refresh();
 
-        $(sel.name).setValue('LadyBug007');
-        $$(sel.radioButtons)[1].click();
-        $(sel.age).setValue('1234567890');
-        $(sel.storyType).click();
-        $$(sel.storyList)[6].click();
-        $(sel.submit).click();
+        inputValues4(name.default, gender.she, age.default, story.comedy);
 
+        $(sel.submit).click();
         browser.pause(2000);
         let tryAgainBtn = $(sel.tryAgain).isDisplayed();
         expect(tryAgainBtn).toEqual(true);
